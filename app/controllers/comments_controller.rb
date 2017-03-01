55db89f68db058a86b76a3067ba3f4efe2d4ba7f
Jeffrey Patajo <jpatajo@gmail.com>
2017-03-01 20:35:11 +0000

    31st commit - Enabled list comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-
+ 
   def create
     @place = Place.find(params[:place_id])
     @place.comments.create(comment_params.merge(user: current_user))
@@ -12,4 +12,6 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:message, :rating)
   end
+
+
 end
