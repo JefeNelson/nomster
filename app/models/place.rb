6a8be83ec79fc38d256d40b8c52a60c9ecc71009
Jeffrey Patajo <jpatajo@gmail.com>
2017-03-01 06:23:51 +0000

    28th commit - Added comments database

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,12 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
 
   validates :name, length: { minimum: 4 }
   validates :name, :address, :description, presence: true
+
 
 end
