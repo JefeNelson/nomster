a016159f06f150e88e7cae6c31eab3cdc177179e
Jeffrey Patajo <jpatajo@gmail.com>
2017-03-06 11:51:40 +0000

    40th commit - Added photos to index and fixed delete error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170305102743) do
+ActiveRecord::Schema.define(version: 20170306113533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20170305102743) do
     t.integer  "user_id"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "picture"
     t.index ["user_id"], name: "index_places_on_user_id", using: :btree
   end
 
